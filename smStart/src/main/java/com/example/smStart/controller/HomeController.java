d2158554a1967e6a5d1e7ed1d7e150ac6323e10f
Sung Min <wings425@naver.com>
2025-03-03 23:59:59 +0900

    메인커밋 테스트

--- smStart/src/main/java/com/example/smStart/controller/HomeController.java
+++ smStart/src/main/java/com/example/smStart/controller/HomeController.java
@@ -23,6 +23,9 @@ public class HomeController {
         List<UserInfo> test =  userInfoService.getAllUsers();
 
         log.info("호출 !!!!!!!!!!!!{}!",test);
+        log.info("호출 !!!!!!!!!!!!{}!",test);
+        log.info("호출 !!!!!!!!!!!!{}!",test);
+        log.info("호출 !!!!!!!!!!!!{}!",test);
         return "home.html";
 
     }
