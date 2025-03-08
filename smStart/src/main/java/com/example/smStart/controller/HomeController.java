a01509401b16d397e95dd1b6832f6fc1ccc37be6
Jo-sung-min <wings425@naver.com>
2025-03-08 15:56:47 +0900

    홈커밋 테스트

--- smStart/src/main/java/com/example/smStart/controller/HomeController.java
+++ smStart/src/main/java/com/example/smStart/controller/HomeController.java
@@ -25,7 +25,6 @@ public class HomeController {
         log.info("호출 !!!!!!!!!!!!{}!",test);
         log.info("호출 !!!!!!!!!!!!{}!",test);
         log.info("호출 !!!!!!!!!!!!{}!",test);
-        log.info("호출 !!!!!!!!!!!!{}!",test);
         return "home.html";
 
     }
